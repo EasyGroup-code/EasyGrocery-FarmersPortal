dfdeb05d490eae97e7caefa7b907054034d66010
EasyGroup-code <sakibarkate786@gmail.com>
2021-02-26 12:30:29 +0530

    first commit

---
+++ backend/data.jsx
@@ -0,0 +1,94 @@
+export default  {
+    product:[
+        {
+        _id:'123',
+        name:'platinum rice',
+        category:'rice',
+        image:'/images/rice1.jpg',
+        price:' 600 Rs',
+        brand:'Badhai',
+        rating:4.5,
+        numReview:10
+    },
+    {
+        _id:'345',
+        name:'Basmati rice',
+        category:'rice',
+        image:'/images/d2.jpg',
+         price:' 700 Rs',
+        brand:'Al Abrar',
+        rating:4.1,
+        numReview:12
+    },
+    {
+        _id:'678',
+        name:'Basmati rice',
+        category:'rice',
+        image:'/images/wheat.jpg',
+        price:'1000 Rs',
+        brand:'India Gate',
+        rating:4.0,
+        numReview:20
+    },
+    {
+        _id:'910',
+        name:'platinum rice',
+        category:'rice',
+        image:'/images/rice1.jpg',
+        price:' 600 Rs',
+        brand:'Badhai',
+        rating:4.5,
+        numReview:10
+    },
+    {
+        _id:'112',
+        name:'Basmati rice',
+        category:'rice',
+        image:'/images/d2.jpg',
+         price:' 700 Rs',
+        brand:'Al Abrar',
+        rating:4.1,
+        numReview:12
+    },
+    {
+        _id:'113',
+        name:'Basmati rice',
+        category:'t-shirt',
+        image:'/images/wheat.jpg',
+        price:'1000 Rs',
+        brand:'India Gate',
+        rating:4.0,
+        numReview:20
+    },
+    {
+        _id:'114',
+        name:'platinum rice',
+        category:'rice',
+        image:'/images/rice1.jpg',
+        price:' 600 Rs',
+        brand:'Nike',
+        rating:4.5,
+        numReview:10
+    },
+    {
+        _id:'115',
+        name:'Basmati rice',
+        category:'rice',
+        image:'/images/wheat.jpg',
+         price:' 1000 Rs',
+        brand:'India Gate',
+        rating:4.1,
+        numReview:12
+    },
+    {
+        _id:'116',
+        name:'Platinum rice ',
+        category:'t-shirt',
+        image:'/images/rice1.jpg',
+        price:'700 Rs',
+        brand:'Badhai',
+        rating:4.0,
+        numReview:20
+    },
+]
+}
